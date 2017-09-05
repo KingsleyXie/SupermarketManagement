cfcc519215324b64b59c4e86cf608fac113e4e02
Kingsley <kingsley.xym@gmail.com>
2017-09-05 20:42:31 +0800

    Finish Formating Code in `finance.js`

--- assets/js/finance.js
+++ assets/js/finance.js
@@ -1,72 +1,63 @@
-var dest = 4, operation = 2, data = {};
-
-var date = new Date();
-var year = date.getFullYear(), month = date.getMonth() + 1, day = date.getDate();
-var time = year + "-" + month + "-" + day;
+$("#loading").css('display', 'flex');
+var date = new Date(),
+	year = date.getFullYear(),
+	month = date.getMonth() + 1,
+	day = date.getDate();
 
 $(document).ready(function() {
+	$(".modal").modal();
+	$("select").material_select();
 	$(".button-collapse").sideNav();
-	$('.modal').modal();
-	$('select').material_select();
 	
-	$('#finance').submit(function(e) {
+	display(); $("#loading").hide();
+
+	$("#finance").submit(function(e) {
 		e.preventDefault();
-		data = {};
-		$(this).serializeArray().map(function(x){data[x.name] = x.value;}); 
-		data["dest"] = dest; data["operation"] = operation;
-		data["year"] = parseFloat(year);
-		data["month"] = parseFloat(month);
-		data["day"] = parseFloat(day);
+
+		data = {'dest': 4, 'operation': 2, 'year': year, 'month': month, 'day': day};
+		$(this).serializeArray().map(function(x){data[x.name] = x.value;});
+		data['income'] = parseFloat(data['income']);
+		data['expenditure'] = parseFloat(data['expenditure']);
 		data = JSON.stringify(data);
 
 		$.ajax({
-			type: "POST",
+			type: 'POST',
 			url: './assets/API/api.cgi',
-			contentType: "application/json; charset=utf-8",
+			contentType: 'application/json; charset=utf-8',
 			data: data,
-			dataType: "json",
-			success: function(response)
-			{
+			success: function(response) {
 				if (response.code == 0) {
-					if (operation == 2) {
-						Materialize.toast("财务流水添加成功！", 3000);
-					}
-
-					window.setTimeout(function ()
-					{
-						window.location.href = "./finance";
-					}, 3600);
+					Materialize.toast('财务流水添加成功！', 1700);
+					window.setTimeout(function () {
+						$("#finance").modal('close');
+						display();
+					}, 2000);
 				}
 			}
 		});
 	});
+});
 
-
-
-	data = {};
-	data["dest"] = dest; data["operation"] = 1;
-	data = JSON.stringify(data);
-
+function display() {
 	$.ajax({
-		type: "POST",
-		url: "./assets/API/api.cgi",
-		contentType: "application/json; charset=utf-8",
-		data: data,
-		dataType: "json",
-		success: function(response)
-		{
-			for (var i = 0; i <= response.length - 1; i++) {
-				document.getElementById("display").innerHTML += 
-					"<tr id=\"ID" + i + "\">" +
-						"<th>" + i + "</th>" + 
-						"<th>" + response[i].name + "</th>" + 
-						"<th>" + response[i].income + "</th>" + 
-						"<th>" + response[i].expenditure + "</th>" + 
-						"<th>" + response[i].date.year + "</th>" + 
-						"<th>" + response[i].date.month + "</th>" + 
-						"<th>" + response[i].date.day + "</th>" + 
-					"</tr>";
+		type: 'POST',
+		url: './assets/API/api.cgi',
+		contentType: 'application/json; charset=utf-8',
+		data: JSON.stringify({'dest': 4, 'operation': 1}),
+		success: function(response) {
+			$("#display").html('');
+			for (var i = 0; i < response.length; i++) {
+				$("#display").append(
+					'<tr>' +
+						'<td>' + i + '</td>' +
+						'<td>' + response[i].name + '</td>' +
+						'<td>' + response[i].income + '</td>' +
+						'<td>' + response[i].expenditure + '</td>' +
+						'<td>' + response[i].date.year + '</td>' +
+						'<td>' + response[i].date.month + '</td>' +
+						'<td>' + response[i].date.day + '</td>' +
+					'</tr>');
 			}
 		}
 	});
-});
+}
