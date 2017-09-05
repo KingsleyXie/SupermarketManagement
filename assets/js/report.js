91bca8e7574f3643054fcf0c037459d4a9193706
Kingsley <kingsley.xym@gmail.com>
2017-09-05 18:44:32 +0800

    Finish Formating Code in `report.js`

--- assets/js/report.js
+++ assets/js/report.js
@@ -38,39 +38,37 @@ $(document).ready(function() {
 		data: JSON.stringify({'dest': 5, 'operation': 1}),
 		success: function(response) {
 			var data = [[], [], [], []];
-			for (var i = 0; i <= response.length - 1; i++) {
-				if (response[i].date.day == day) {
-					data[0].push({  y: response[i].income, indexLabel: response[i].name });
-					data[1].push({  y: response[i].expenditure, indexLabel: response[i].name });
-				}
+			for (var i = 0; i < response.length; i++) {
+				var resYear = response[i].date.year,
+					resMonth = response[i].date.month,
+					resDay = response[i].date.day;
 
-				if (response[i].date.month == month) {
-					var dup = data[2].filter(function(obj) {
-						return obj.x == response[i].date.day;
-					});
+				if (resYear == year && resMonth == month) {
+					//Check If The Data Of Current Day Had Been Set
+					var dup = data[2].filter(function(obj) { return obj.x == resDay; });
+					//Create A New Array Element With Current Data And Date
+					dup.length == 0 ? data[2].push({ x: resDay, y: response[i].income, label: resDay }) :
+					//Or Add Current Data With Previous One Of The Same Date
+					data[2][data[2].map(function(d) { return d['x']; }).indexOf(dup[0].x)].y += response[i].income;
 
-					if (dup.length == 0) {
-						data[2].push({ x: response[i].date.day, y: parseFloat(response[i].income), label: response[i].date.day });
-					}
-					else {
-						data[2][data[2].map(function(d) { return d['x']; }).indexOf(dup[0].x)].y += parseFloat(response[i].income);
-					}
+					var dup = data[3].filter(function(obj) { return obj.x == resDay; });
+					dup.length == 0 ? data[3].push({ x: resDay, y: response[i].expenditure, label: resDay }) :
+					data[3][data[3].map(function(d) { return d['x']; }).indexOf(dup[0].x)].y += response[i].expenditure;
 
-					var dup = data[3].filter(function(obj) {
-						return obj.x == response[i].date.day;
-					});
+					if (resDay == day) {
+						//Add Income And Expenditure Data Of Current Date
+						response[i].income != 0 ?
+						data[0].push({y: response[i].income, indexLabel: response[i].name}) : '';
 
-					if (dup.length == 0) {
-						data[3].push({ x: response[i].date.day, y: parseFloat(response[i].expenditure), label: response[i].date.day });
-					}
-					else {
-						data[3][data[3].map(function(d) { return d['x']; }).indexOf(dup[0].x)].y += parseFloat(response[i].expenditure);
+						response[i].expenditure != 0 ?
+						data[1].push({y: response[i].expenditure, indexLabel: response[i].name}) : '';
 					}
 				}
 			}
 
 			$("#loading").hide();
 			for (var i = 0; i < 4; i++) {
+				//Set Data To Charts And Render Them
 				chart[i].options.title.fontWeight = 'normal';
 				chart[i].options.data[0].dataPoints = data[i];
 				chart[i].render();
