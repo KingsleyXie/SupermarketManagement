55f1dde84d382b3b9ce2993cfc545dc9a17db982
Kingsley <kingsley.xym@gmail.com>
2017-09-06 22:51:06 +0800

    init Reorganize Inventory Module

--- assets/js/inventory.js
+++ assets/js/inventory.js
@@ -1,27 +1,35 @@
-var dest = 2, operation = 2, data = {};
+$("#loading").css('display', 'flex');
+display(); $("#loading").hide();
+var modifying = false;
 
-var date = new Date();
-var year = date.getFullYear(), month = date.getMonth() + 1, day = date.getDate();
-var time = year + "-" + month + "-" + day;
+var date = new Date(),
+	year = date.getFullYear(),
+	month = date.getMonth() + 1,
+	day = date.getDate(),
+	time = year + '-' + month + '-' + day;
 
 $(document).ready(function() {
-	$('.button-collapse').sideNav();
-	$('.modal').modal();
-	$('select').material_select();
-	$('.datepicker').pickadate({
+	$(".modal").modal();
+	$("select").material_select();
+	$(".button-collapse").sideNav();
+	$(".datepicker").pickadate({
     	selectMonths: true,
     	selectYears: 15,
-    	format:'yyyy-mm-dd'
+    	format: 'yyyy-mm-dd'
     });
-	
-	$('#add').submit(function(e) {
+
+	$("#inventory").submit(function(e) {
 		e.preventDefault();
-		data = {};
-		$(this).serializeArray().map(function(x){data[x.name] = x.value;}); 
-		data["dest"] = dest; data["operation"] = operation;
-		data["year"] = year; data["month"] = month;
-		data["day"] = day; data["time"] = time;
-		data["itemID"] = parseInt(document.getElementById("itemID").value);
+		data = {
+			"dest": 2,
+			"operation": (modifying ? 3 : 2),
+			"year": year,
+			"month": month,
+			"day": day,
+			"time": time,
+			"itemID": parseInt($("#itemID").val())
+		};
+		$(this).serializeArray().map(function(x){data[x.name] = x.value;});
 		data["supplierID"] = parseInt(data["supplierID"]);
 		data["inventoryQuantity"] = parseInt(data["inventoryQuantity"]);
 		data["threshold"] = parseInt(data["threshold"]);
@@ -30,71 +38,31 @@ $(document).ready(function() {
 		data = JSON.stringify(data);
 
 		$.ajax({
-			type: "POST",
+			type: 'POST',
 			url: './assets/API/api.cgi',
-			contentType: "application/json; charset=utf-8",
+			contentType: 'application/json; charset=utf-8',
 			data: data,
-			dataType: "json",
-			success: function(response)
-			{
+			success: function(response) {
 				if (response.code == 0) {
-					if (operation == 2) {
-						Materialize.toast("货物添加成功！", 3000);
-					}
-					if (operation == 3) {
-						Materialize.toast("货物信息修改成功！", 3000);
-					}
-
-					window.setTimeout(function ()
-					{
-						window.location.href = "./inventory";
-					}, 3600);
+					Materialize.toast('货物信息' + (modifying ? '修改' : '添加') + '成功！', 1700);
+					setTimeout(function () {
+						$("#inventory").modal('close');
+						display();
+						
+						//Reset All Input Data
+						$("input").val('');
+						$("select").val(0).material_select();
+						$("label").removeClass("active");
+					}, 2000);
 				}
 			}
 		});
 	});
-
-	data = {};
-	data["dest"] = dest; data["operation"] = 1;
-	data = JSON.stringify(data);
-
-	$.ajax({
-		type: "POST",
-		url: "./assets/API/api.cgi",
-		contentType: "application/json; charset=utf-8",
-		data: data,
-		dataType: "json",
-		success: function(response)
-		{
-			for (var i = 0; i <= response.length - 1; i++) {
-				document.getElementById("display").innerHTML += 
-					"<tr id=\"ID" + i + "\">" +
-						"<th>" + i + "</th>" + 
-						"<th>" + response[i].barcode + "</th>" + 
-						"<th>" + response[i].brand + "</th>" + 
-						"<th>" + response[i].name + "</th>" + 
-						"<th>" + response[i].unspsc + "</th>" + 
-						"<th>" + response[i].type + "</th>" + 
-						"<th>" + response[i].price + "</th>" + 
-						"<th>" + response[i].salePrice + "</th>" + 
-						"<th>" + response[i].inventoryQuantity + "</th>" + 
-						"<th>" + response[i].threshold + "</th>" + 
-						"<th>" + response[i].expiredTime + "</th>" + 
-						"<th>" + response[i].importTime + "</th>" + 
-						"<th>" + response[i].updateTime + "</th>" + 
-					"</tr>";
-				if (response[i].inventoryQuantity < response[i].threshold) {
-					$("#ID" + i).addClass("red");
-				}
-			}
-		}
-	});
 });
 
-
 function inputData() {
-	document.getElementById("addInit").style.display = "none";
-	document.getElementById("itemInfo").style.display = "block";
+	document.getElementById("add-init").style.display = "none";
+	document.getElementById("item-info").style.display = "block";
 }
 
 function getData() {
@@ -110,69 +78,87 @@ function getData() {
         // https://market.aliyun.com/products/56928004/cmapi011806.html
       },
       data: 'barcode=' + barcode,
-      success: function(response)
-      {
+      success: function(response) {
       	if (response.status == 0) {
-				document.querySelector("#brand + label").className ="active";
 				document.getElementById("brand").value = response.result.brand;
-				document.querySelector("#name + label").className ="active";
 				document.getElementById("name").value = response.result.name;
-				document.querySelector("#unspsc + label").className ="active";
 				document.getElementById("unspsc").value = response.result.unspsc;
-				document.querySelector("#type + label").className ="active";
 				document.getElementById("type").value = response.result.type;
-				document.querySelector("#price + label").className ="active";
 				document.getElementById("price").value = response.result.price;
 
 				window.setTimeout(function ()
 				{
-					document.getElementById("inventoryQuantity").focus();
+					document.getElementById("inventory-quantity").focus();
 				}, 0);
 
 				document.getElementById("progress").style.display = "none";
-				document.getElementById("addInit").style.display = "none";
-				document.getElementById("itemInfo").style.display = "block";
+				document.getElementById("add-init").style.display = "none";
+				document.getElementById("item-info").style.display = "block";
 			}},
 		error: function() {
 			Materialize.toast("未找到商品信息，请手动录入数据", 3000);
 			document.getElementById("progress").style.display = "none";
-			document.getElementById("addInit").style.display = "none";
-			document.getElementById("itemInfo").style.display = "block";
+			document.getElementById("add-init").style.display = "none";
+			document.getElementById("item-info").style.display = "block";
 		}
 	});
 }
 
 function update() {
+	modifying = true;
 	var info = document.getElementById("ID" + document.getElementById("itemID").value).cells;
 
-	document.getElementById("inventoryHeader").textContent = "修改货物信息";
-	document.getElementById("inventoryBtn").textContent = "确认修改";
+	document.getElementById("inventory-header").textContent = "修改货物信息";
+	document.getElementById("btn-inventory").textContent = "确认修改";
 	
-	document.querySelector("#brand + label").className ="active";
 	document.getElementById("brand").value = info[2].textContent;
-	document.querySelector("#name + label").className ="active";
 	document.getElementById("name").value = info[3].textContent;
-	document.querySelector("#unspsc + label").className ="active";
 	document.getElementById("unspsc").value = info[4].textContent;
-	document.querySelector("#type + label").className ="active";
 	document.getElementById("type").value = info[5].textContent;
-	document.querySelector("#price + label").className ="active";
 	document.getElementById("price").value = info[6].textContent;
-	document.querySelector("#salePrice + label").className ="active";
-	document.getElementById("salePrice").value = info[7].textContent;
-	document.querySelector("#inventoryQuantity + label").className ="active";
-	document.getElementById("inventoryQuantity").value = info[8].textContent;
+	document.getElementById("sale-price").value = info[7].textContent;
+	document.getElementById("inventory-quantity").value = info[8].textContent;
 
-	$('#expiredTime').pickadate('picker').set('select', info[10].textContent, { format: 'yyyy-mm-dd' }).trigger("change");
+	$('#expired-time').pickadate('picker').set('select', info[10].textContent).trigger("change");
 
 	$("#search").modal("close");
-	$("#add").modal("open");
+	$("#inventory").modal("open");
 	inputData();
 
-	window.setTimeout(function ()
-	{
-		document.getElementById("inventoryQuantity").focus();
+	setTimeout(function () {
+		$("#inventory-quantity").focus();
 	}, 0);
+}
 
-	operation = 3;
+function display() {
+	$.ajax({
+		type: 'POST',
+		url: './assets/API/api.cgi',
+		contentType: 'application/json; charset=utf-8',
+		data: JSON.stringify({"dest": 2, "operation": 1}),
+		success: function(response) {
+			$("#display").html('');
+			for (var i = 0; i < response.length; i++) {
+				$("#display").append(
+				'<tr id="ID' + i + '">' +
+					'<th>' + i + '</th>' + 
+					'<th>' + response[i].barcode + '</th>' + 
+					'<th>' + response[i].brand + '</th>' + 
+					'<th>' + response[i].name + '</th>' + 
+					'<th>' + response[i].unspsc + '</th>' + 
+					'<th>' + response[i].type + '</th>' + 
+					'<th>' + response[i].price + '</th>' + 
+					'<th>' + response[i].salePrice + '</th>' + 
+					'<th>' + response[i].inventoryQuantity + '</th>' + 
+					'<th>' + response[i].threshold + '</th>' + 
+					'<th>' + response[i].expiredTime + '</th>' + 
+					'<th>' + response[i].importTime + '</th>' + 
+					'<th>' + response[i].updateTime + '</th>' + 
+				'</tr>');
+				if (response[i].inventoryQuantity < response[i].threshold) {
+					$("#ID" + i).addClass("red lighten-1");
+				}
+			}
+		}
+	});
 }
