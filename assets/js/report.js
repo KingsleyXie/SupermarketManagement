197496cfbc6133a145f386cba8a6e7e1a8d823a4
Kingsley <kingsley.xym@gmail.com>
2017-09-07 18:29:54 +0800

    Optimize Report Chart Render And Display Method

--- assets/js/report.js
+++ assets/js/report.js
@@ -6,7 +6,7 @@ var date = new Date(),
 	day = date.getDate();
 
 var chart = [
-	new CanvasJS.Chart("chart1", {
+	new CanvasJS.Chart("chart0", {
 		animationEnabled: true,
 		title: { text: year + '年' + month + '月' + day + '日收入报表' },
 		subtitles: [{
@@ -17,7 +17,7 @@ var chart = [
 		data: [{ type: 'doughnut' }]
 	}),
 
-	new CanvasJS.Chart("chart2", {
+	new CanvasJS.Chart("chart1", {
 		animationEnabled: true,
 		title: { text: year + '年' + month + '月' + day + '日支出报表' },
 		subtitles: [{
@@ -27,15 +27,27 @@ var chart = [
 		}],
 		data: [{ type: 'doughnut' }]
 	}),
+	
+	'',
 
 	new CanvasJS.Chart("chart3", {
+		animationEnabled: true,
+		zoomEnabled:true,
 		title: { text: year + '年' + month + '月收入报表' },
-		data: [{ type: 'column' }]
+		data: [{
+			type: 'column',
+			indexLabel: "{y}"
+		}]
 	}),
 
 	new CanvasJS.Chart("chart4", {
+		animationEnabled: true,
+		zoomEnabled:true,
 		title: { text: year + '年' + month + '月支出报表' },
-		data: [{ type: 'column' }]
+		data: [{
+			type: 'column',
+			indexLabel: "{y}"
+		}]
 	})
 ];
 
@@ -47,10 +59,10 @@ $(document).ready(function() {
 		data: JSON.stringify({"dest": 5, "operation": 1}),
 		success: function(response) {
 			//Initialize `data` Array and Data Of Monthly Report Chart
-			var data = [[], [], [], []];
+			var data = [[], [], [], [], [], []];
 			for (var i = 1; i <= day; i++) {
-				data[2].push({ x: i, y: 0, label: i });
 				data[3].push({ x: i, y: 0, label: i });
+				data[4].push({ x: i, y: 0, label: i });
 			}
 
 			for (var i = 0; i < response.length; i++) {
@@ -60,8 +72,8 @@ $(document).ready(function() {
 
 				if (resYear == year && resMonth == month) {
 					//Summarize Monthly Report Data
-					data[2][resDay - 1].y += response[i].income;
-					data[3][resDay - 1].y += response[i].expenditure;
+					data[3][resDay - 1].y += response[i].income;
+					data[4][resDay - 1].y += response[i].expenditure;
 
 					if (resDay == day) {
 						//Add Income And Expenditure Data Of Current Date
@@ -69,7 +81,7 @@ $(document).ready(function() {
 						data[0].push({ y: response[i].income, indexLabel: response[i].name }) : '';
 
 						response[i].expenditure != 0 ?
-						data[1].push({ y: response[i].expenditure, indexLabel: response[i].name }) : '';
+						data[1].push({ y: response[i].expenditure, indexLabel: response[i].name }) : '';						
 					}
 				}
 			}
@@ -78,11 +90,16 @@ $(document).ready(function() {
 			if (data[1].length) chart[1].options.subtitles[0].text = '';
 
 			$("#loading").hide();
-			for (var i = 0; i < 4; i++) {
+			for (let i = 0; i < 5; i++) {
+				if (i == 2) i++;
 				//Set Data To Charts And Render Them
 				chart[i].options.title.fontWeight = 'normal';
 				chart[i].options.data[0].dataPoints = data[i];
-				chart[i].render();
+				$("#chart" + i).one('inview', function(event, isInView) {
+					if (isInView) {
+						chart[i].render();
+					}
+				});
 			}
 		}
 	});
