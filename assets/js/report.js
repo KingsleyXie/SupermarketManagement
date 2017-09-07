5823bb57943cd172543fa3ac2276433511f4a794
Kingsley <kingsley.xym@gmail.com>
2017-09-07 12:35:49 +0800

    Optimize Report Javascript Code

--- assets/js/report.js
+++ assets/js/report.js
@@ -8,24 +8,34 @@ var date = new Date(),
 var chart = [
 	new CanvasJS.Chart("chart1", {
 		animationEnabled: true,
-		title: {text: year + '年' + month + '月' + day + '日收入报表'},
-		data: [{type: 'doughnut'}]
+		title: { text: year + '年' + month + '月' + day + '日收入报表' },
+		subtitles: [{
+			text: '（暂无当天收入数据）',
+			fontSize: 30,
+			verticalAlign: 'center'
+		}],
+		data: [{ type: 'doughnut' }]
 	}),
 
 	new CanvasJS.Chart("chart2", {
 		animationEnabled: true,
-		title: {text: year + '年' + month + '月' + day + '日支出报表'},
-		data: [{type: 'doughnut'}]
+		title: { text: year + '年' + month + '月' + day + '日支出报表' },
+		subtitles: [{
+			text: '（暂无当天支出数据）',
+			fontSize: 30,
+			verticalAlign: 'center'
+		}],
+		data: [{ type: 'doughnut' }]
 	}),
 
 	new CanvasJS.Chart("chart3", {
-		title: {text: year + '年' + month + '月收入报表'},
-		data: [{type: 'column'}]
+		title: { text: year + '年' + month + '月收入报表' },
+		data: [{ type: 'column' }]
 	}),
 
 	new CanvasJS.Chart("chart4", {
-		title: {text: year + '年' + month + '月支出报表'},
-		data: [{type: 'column'}]
+		title: { text: year + '年' + month + '月支出报表' },
+		data: [{ type: 'column' }]
 	})
 ];
 
@@ -36,34 +46,36 @@ $(document).ready(function() {
 		contentType: 'application/json; charset=utf-8',
 		data: JSON.stringify({"dest": 5, "operation": 1}),
 		success: function(response) {
+			//Initialize `data` Array and Data Of Monthly Report Chart
 			var data = [[], [], [], []];
+			for (var i = 1; i <= day; i++) {
+				data[2].push({ x: i, y: 0, label: i });
+				data[3].push({ x: i, y: 0, label: i });
+			}
+
 			for (var i = 0; i < response.length; i++) {
 				var resYear = response[i].date.year,
 					resMonth = response[i].date.month,
 					resDay = response[i].date.day;
 
 				if (resYear == year && resMonth == month) {
-					//Check If The Data Of Current Day Had Been Set
-					var dup = data[2].filter(function(obj) { return obj.x == resDay; });
-					//Create A New Array Element With Current Data And Date
-					dup.length == 0 ? data[2].push({ x: resDay, y: response[i].income, label: resDay }) :
-					//Or Add Current Data With Previous One Of The Same Date
-					data[2][data[2].map(function(d) { return d['x']; }).indexOf(dup[0].x)].y += response[i].income;
-
-					var dup = data[3].filter(function(obj) { return obj.x == resDay; });
-					dup.length == 0 ? data[3].push({ x: resDay, y: response[i].expenditure, label: resDay }) :
-					data[3][data[3].map(function(d) { return d['x']; }).indexOf(dup[0].x)].y += response[i].expenditure;
+					//Summarize Monthly Report Data
+					data[2][resDay - 1].y += response[i].income;
+					data[3][resDay - 1].y += response[i].expenditure;
 
 					if (resDay == day) {
 						//Add Income And Expenditure Data Of Current Date
 						response[i].income != 0 ?
-						data[0].push({y: response[i].income, indexLabel: response[i].name}) : '';
+						data[0].push({ y: response[i].income, indexLabel: response[i].name }) : '';
 
 						response[i].expenditure != 0 ?
-						data[1].push({y: response[i].expenditure, indexLabel: response[i].name}) : '';
+						data[1].push({ y: response[i].expenditure, indexLabel: response[i].name }) : '';
 					}
 				}
 			}
+
+			if (data[0].length) chart[0].options.subtitles[0].text = '';
+			if (data[1].length) chart[1].options.subtitles[0].text = '';
 
 			$("#loading").hide();
 			for (var i = 0; i < 4; i++) {
